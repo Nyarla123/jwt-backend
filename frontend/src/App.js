18c489dfd4ccd743f7c3c25722e9c855e1fcc65d
[raul] <rksekf456@gmail.com>
2022-03-07 09:25:57 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React,{useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
 
 function App() {
 
@@ -17,22 +23,26 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            {message}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className="App">
+              <Navbar />
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                      {message}
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React
+                  </a>
+              </header>
+          </div>
+      </Router>
+
   );
 }
 
